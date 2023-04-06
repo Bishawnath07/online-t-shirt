83cdf0ea842ce8fd3883894fbcb76e61aaaa1451
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-04-06 23:37:39 +0600

    initial data load

---
+++ src/Components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>Order Reviews</h3>
+        </div>
+    );
+};
+
+export default OrderReview;
